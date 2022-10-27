31c44cdf483baa9c0d539015cc4bbc49088b6714
Allister Gough <allistergough02@gmail.com>
2022-10-27 14:32:49 +0100

    Finish solution

--- primes.py
+++ primes.py
@@ -3,4 +3,9 @@
 
 def primes(number_of_primes):
     list = []
+    with open('primes.txt', 'r') as p:
+        for i in range(number_of_primes):
+            prime = int(p.readline())
+            list.append(prime)
+
     return list
